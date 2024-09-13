723b46fb7ca27e828ccf3986a908543af9864dd5
Moibrahi7 <moibrahi7@gmail.com>
2024-09-13 12:42:58 -0400

    Feat: Added Nav paths and cleaned up code

--- cdyearbook/src/App.js
+++ cdyearbook/src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import { GradListGrid } from './components/gradList/grid/GradListGrid';
-import { GradListCarousol } from './components/gradList/carasol/GradListCarousol';
+import { GradPage } from './pages/gradpage/GradPage.jsx';
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Home from './pages/landing/Home.jsx';
+import { Header } from './components/Header/Header.jsx';
 
 function App() {
   return (
     <div className="App">
-      <GradListCarousol/>
-      <GradListGrid/>
+      <Header />
+      <BrowserRouter>
+      <Routes>
+        <Route path='/:cohort' exact element= {<GradPage/>} />
+        <Route path='/' exact element= {<Home/>} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
