cf16e43320e08842d7502b69abc7b03917ca0cd5
Moibrahi7 <moibrahi7@gmail.com>
2024-09-16 17:16:15 -0400

    Chore: Placed old content into old page folder and placed react content in the main page

--- src/components/gradList/carasol/GradListCarousol.jsx
+++ src/components/gradList/carasol/GradListCarousol.jsx
@@ -1,30 +1,10 @@
 import React from "react";
 import "./GradListCarousol.css";
 import Carousel from 'react-material-ui-carousel'
-import { Paper, Button } from '@mui/material'
+import { Paper } from '@mui/material'
 import { useParams } from "react-router-dom";
 import data from "../../../resources/yearEntries.json";
 
-
-const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1
-  }
-};
 const findCohortById = (cohort) => {
   return data.find(co => co.cohort === cohort);
 };
