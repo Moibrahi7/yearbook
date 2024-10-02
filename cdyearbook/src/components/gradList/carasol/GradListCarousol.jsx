e19037778eaca3fa26866fc6c4ce58d09643939e
Moibrahi7 <moibrahi7@gmail.com>
2024-10-02 13:22:22 -0400

    Fix: Issue with npm run dev resolved

--- cdyearbook/src/components/gradList/carasol/GradListCarousol.jsx
+++ cdyearbook/src/components/gradList/carasol/GradListCarousol.jsx
@@ -11,10 +11,25 @@ const findCohortById = (cohort) => {
 export function GradListCarousol (){
   let { cohort } = useParams()
   const coData = findCohortById(cohort)
+  let backImg = "/img/${coData.entries[0].img}"
     return(
         <>
           <Carousel autoPlay = {false} animation = "slide" height={500} className="carousol bg-gray-800" navButtonsAlwaysVisible = {true} indicators = {false}>  
             {coData.entries.map((gradInfo) => (
+              <>
+                {/* <div 
+                  className="grad-card"
+                  style={{
+                    backgroundImage: `url("/img/${gradInfo.img}")`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat',
+                    height: '100%',
+                    width: '100%',
+                    position: 'relative',
+                    alignSelf: 'center',
+                  }}
+                /> */}
               <Paper>
                 <div className= "grad-card-c">
                   <li>
@@ -24,6 +39,7 @@ export function GradListCarousol (){
                   </li>
                 </div>
               </Paper>
+              </>
             ))}
           </Carousel>
         </>
