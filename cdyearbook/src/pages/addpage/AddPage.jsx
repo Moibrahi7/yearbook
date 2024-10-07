7c1d152732148f05bad4e1b13db6062877043637
Moibrahi7 <moibrahi7@gmail.com>
2024-10-07 10:54:03 -0400

    Feat: added better styling to add page

--- cdyearbook/src/pages/addpage/AddPage.jsx
+++ cdyearbook/src/pages/addpage/AddPage.jsx
@@ -13,25 +13,42 @@ export default function AddPage() {
     const coData = findCohortById(cohort)
     const [name, setName] = useState("");
     const [qoute, setQoute] = useState("");
-    const [img, setImg] = useState("");
+    const [img, setImg] = useState("anothony.jpeg");
+
+    let inStyle = {
+        borderColor: ""
+    }
+    // const clearFields = () => {
+    //     setName("");
+    //     setQoute("");
+    //     setImg("anothony.jpeg");
+    // }
 
     return (
+        <>
         <div className="add-form">
             <h1>Add Grad</h1>
             <form>
                 <div className="form-group">
                     <label>Name</label>
                     <br/>
-                    <input type="text" id ='name' onChange={(e) => setName(e.target.value)}/>
+                    <input type="text" id ='name-field' className='form-control' style={inStyle} onChange={(e) => setName(e.target.value)}/>
                 </div>
                 <div className="form-group">
                     <label>Qoute</label>
                     <br/>
-                    <input type="text" id='qoute' onChange={(e) => setQoute(e.target.value)}/>
+                    <input type="text" id='qoute-field' className='form-control' style={inStyle} onChange={(e) => setQoute(e.target.value)}/>
                 </div>
             </form>
-            <button type="submit" className="btn" onClick={() => {data.find(co => co.cohort === cohort).entries.push({name, qoute, img})}}>Submit</button>
+            <button type="submit" className="btn" onClick={() => {
+                if (!name || !qoute) {
+                    alert("Please fill out all fields");
+                    return;
+                }
+                data.find(co => co.cohort === cohort).entries.push({name, qoute, img, entryNum: data.find(co => co.cohort === cohort).entries.length + 1} 
+                )}}>Submit</button>
             <br/>
         </div>
+        </>
     )
 }
