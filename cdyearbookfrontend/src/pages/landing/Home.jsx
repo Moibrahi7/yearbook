0e827d4292f2c3da54b7344bb58a5facaa600651
Moibrahi7 <160788202+Moibrahi7@users.noreply.github.com>
2025-01-19 13:47:24 -0500

    fix: adding styling directly to the carusol to fix styling issues

--- cdyearbookfrontend/src/pages/landing/Home.jsx
+++ cdyearbookfrontend/src/pages/landing/Home.jsx
@@ -5,21 +5,23 @@ import { Paper } from '@mui/material'
 export default function Home (){
     return(
         <>
-            {yearData.map((info) => (
-             <Carousel autoPlay = {false} animation = "slide" height={500} className="carousol" navButtonsAlwaysVisible = {true} indicators = {false} key={info.cohort} style={{backgroundColor: "gray"}}>  
-                        {info.entries.map((gradInfo) =>(
-                        <Paper key={gradInfo.entryNum}>
-                            <div className= "grad-card-c" key={gradInfo.entryNum}>
-                                <li key={gradInfo.entryNum}>
-                                    <img src={"/img/" + gradInfo.img} className="w-full" alt="Tailwind CSS Carousel component"  />
-                                    <h1>{gradInfo.name}</h1>
-                                    <p>&quot;{gradInfo.quote}&quot;</p>
-                                </li>
-                            </div>
-                        </Paper>
-                    ))}
-                </Carousel>
-            ))}
+            <div className="g-container">
+                {yearData.map((info) => (
+                <Carousel autoPlay = {false} animation = "slide" height={500} className="carousol" navButtonsAlwaysVisible = {true} indicators = {false} key={info.cohort} style={{backgroundColor: "gray"}}>  
+                            {info.entries.map((gradInfo) =>(
+                            <Paper key={gradInfo.entryNum}>
+                                <div className= "grad-card-c" key={gradInfo.entryNum}>
+                                    <li key={gradInfo.entryNum}>
+                                        <img src={"/img/" + gradInfo.img} className="w-full" alt="Tailwind CSS Carousel component"  />
+                                        <h1>{gradInfo.name}</h1>
+                                        <p>&quot;{gradInfo.quote}&quot;</p>
+                                    </li>
+                                </div>
+                            </Paper>
+                        ))}
+                    </Carousel>
+                ))}
+            </div>
         </>
     )
 }
